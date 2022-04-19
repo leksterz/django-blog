456800c789c3403fb871f8323ca668b1c5079e6b
Alex Ablazevics <ablazevix@gmail.com>
2022-04-19 00:07:28 -0400

    added templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-#create a function to handle traffic from homepage
+posts = [
+    {
+        'author': 'Blaze ENT',
+        'title': 'Blog Post #1',
+        'content': 'First post Content babu',
+        'date_posted': 'August 27, 2022'
+    },
+    {
+        'author': 'NANCY ENT',
+        'title': 'Blog Post #2',
+        'content': 'First post Content Lo',
+        'date_posted': 'August 7, 2022'
+    },
+    {
+        'author': 'NANCY ENT',
+        'title': 'Blog Post #2',
+        'content': '2nd post Content Chief',
+        'date_posted': 'August 8, 2022'
+    },
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
