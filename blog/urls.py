1ae2d24b52925f42446c27237d2ed980c2c39868
Alex Ablazevics <ablazevix@gmail.com>
2022-04-25 23:48:18 -0400

    added pagination, + user blogs can be seen on profile

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from .views import (PostListView,
 PostDetailView, 
 PostCreateView,
 PostUpdateView,
-PostDeleteView)
+PostDeleteView,
+UserPostListView)
 from . import views
 
 urlpatterns = [
@@ -11,6 +12,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
